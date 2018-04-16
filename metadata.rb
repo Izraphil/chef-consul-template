59fb5bd56524676dd18cdc08a422e70c12d70b8d
Peter Karbarz <peter.karbarz@rakuten.com>
2018-04-16 18:10:02 -0400

    updating metadata

--- metadata.rb
+++ metadata.rb
@@ -22,6 +22,4 @@ depends 'golang'
 depends 'runit'
 depends 'nssm'
 
-issues_url 'https://github.com/adamkrone/chef-consul-template/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/adamkrone/chef-consul-template' if respond_to?(:source_url)
 chef_version '>= 12.1' if respond_to?(:chef_version)
